e0b1674f2f85ae7a1efdd86e532a91ed52372387
Larsone <larsone.mwf@gmail.com>
2018-04-15 18:52:48 +0100

    Prints questions and answers correctly

--- src/main/java/coursework/App.java
+++ src/main/java/coursework/App.java
@@ -6,7 +6,7 @@ public class App {
 	public static void main(String[] args) {
 		QuestionLib questionLib = new QuestionLib();
 		for (int i = 0; i < questionLib.getMaxQuestions(); i++){
-			System.out.println(questionLib.questionPrint(i));
+			System.out.println((i + 1) + ": " + questionLib.questionPrint(i));
 			System.out.println(questionLib.answersPrint(i));
 		}
 
