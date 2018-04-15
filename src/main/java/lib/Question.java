ef924b989d694415590148029f402302e28ab49d
Larsone <larsone.mwf@gmail.com>
2018-04-15 16:31:54 +0100

    Correct question answer

--- src/main/java/lib/Question.java
+++ src/main/java/lib/Question.java
@@ -1,16 +1,28 @@
 package lib;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Question {
     HashMap<String, String> map;
+    String correctAnswer;
 
 
     public Question(HashMap<String, String> map){
         this.map = map;
+        setCorrectAnswer();
     }
 
-    // correct answer
+    // Set the correct answer
+    public void setCorrectAnswer(){
+        // Pick a random number between 0-2
+        int r = (int) (Math.random() * (3));
+        // Get a list of all the kets
+        ArrayList<String> keys = new ArrayList<String>(map.keySet());
+        // Set the correct answer using the random number
+        correctAnswer = keys.get(r);
+    }
+
     // user input
     // choose an answer
     // country
