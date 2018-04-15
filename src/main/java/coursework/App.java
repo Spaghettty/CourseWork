26b48635edd52be7d997865b28cc16ecef25d6d4
Larsone <larsone.mwf@gmail.com>
2018-04-15 15:32:25 +0100

    Return type hashmap

--- src/main/java/coursework/App.java
+++ src/main/java/coursework/App.java
@@ -2,11 +2,17 @@ package coursework;
 
 import coursework.sql.SQLQueries;
 
+import java.util.HashMap;
+
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Ayyyy");
 		SQLQueries sqlQueries = new SQLQueries();
-		sqlQueries.lookup();
+		HashMap<String, String> questions = sqlQueries.getQuestion();
+		for (String key : questions.keySet()){
+			System.out.println("Country: " + key + ", City: "
+					+ questions.get(key));
+		}
 
 	}
 
--- src/main/java/coursework/sql/SQLQueries.java
+++ src/main/java/coursework/sql/SQLQueries.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import org.apache.commons.text.WordUtils;
 
 import java.sql.ResultSet;
+import java.util.HashMap;
 
 /**
  * @author Padraig
@@ -33,24 +34,22 @@ public class SQLQueries {
 	 * 
 	 * @return
 	 */
-	public String lookup() {
+	public HashMap<String, String> getQuestion() {
+		HashMap<String, String> question = new HashMap<String, String>();
 		try {
 			Statement statement = database.getConnection().createStatement();
-			String SQL = "SELECT * FROM capitals ORDER BY RAND() LIMIT 3";
-			ResultSet rs = statement.executeQuery(SQL);
+			String sql = "SELECT * FROM capitals ORDER BY RAND() LIMIT 3";
+			ResultSet rs = statement.executeQuery(sql);
 			while (rs.next()) {
-				System.out.println("Country: " + WordUtils.capitalizeFully(rs.getString(1)) + ", City: "
-						+ WordUtils.capitalizeFully(rs.getString(2)));
+				question.put(WordUtils.capitalizeFully(rs.getString(1)),
+						WordUtils.capitalizeFully(rs.getString(2)));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-
-	
-
-		return null;
+		return question;
 	}
 
 }
