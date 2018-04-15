988204674bc63f44d95cc8679df54381d1cba6f0
Larsone <larsone.mwf@gmail.com>
2018-04-15 17:05:29 +0100

    Adding tests

---
+++ src/main/test/coursework/QuestionTests.java
@@ -0,0 +1,12 @@
+package coursework;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuestionTests {
+
+    @Test
+    public void testSomething(){
+        Assert.assertTrue(true);
+    }
+}
