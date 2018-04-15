fdd671d5580d25648be51cd5f291214df36fb339
Larsone <larsone.mwf@gmail.com>
2018-04-15 16:23:58 +0100

    Question Object

---
+++ src/main/java/lib/Question.java
@@ -0,0 +1,18 @@
+package lib;
+
+import java.util.HashMap;
+
+public class Question {
+    HashMap<String, String> map;
+
+
+    public Question(HashMap<String, String> map){
+        this.map = map;
+    }
+
+    // correct answer
+    // user input
+    // choose an answer
+    // country
+    // cities
+}
