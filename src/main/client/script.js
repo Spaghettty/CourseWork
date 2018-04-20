899e9c2543dce8e2e4aee757a9567e04082990bd
Larsone <larsone.mwf@gmail.com>
2018-04-20 02:43:29 +0100

    Client talking to REST

--- src/main/client/script.js
+++ src/main/client/script.js
@@ -1,39 +1,22 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	// create the module and name it app
+	var app = angular.module('app', []);
 
-	// configure our routes
-	scotchApp.config(function($routeProvider) {
-		$routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
-
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
-
-			// route for the contact page
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
-			});
-	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
+	app.controller('mainController', function($scope, $http) {
+
+
 		// create a message to display in our view
 		$scope.message = 'Everyone come and see how good I look!';
-	});
-
-	scotchApp.controller('aboutController', function($scope) {
-		$scope.message = 'Look! I am an about page.';
-	});
-
-	scotchApp.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
+		$http({method: 'GET', url: 'http://localhost:8080/getScore'}).
+            success(function(data, status, headers, config) {
+              // this callback will be called asynchronously
+              // when the response is available
+              console.log(data);
+            }).
+            error(function(data, status, headers, config) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+            });
 	});
--- src/main/java/coursework/rest/RESTController.java
+++ src/main/java/coursework/rest/RESTController.java
@@ -5,10 +5,7 @@ import coursework.lib.Question;
 import coursework.lib.Answer;
 import coursework.lib.Score;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RESTController {
@@ -16,11 +13,13 @@ public class RESTController {
     @Autowired
     private Game game;
 
+    @CrossOrigin
     @RequestMapping(path="/getQuestion", method=RequestMethod.POST)
     public Question getQuestion(@RequestParam(value="num") int num) {
         return game.getQuestion(num);
     }
 
+    @CrossOrigin
     @RequestMapping(path="/answerQuestion", method=RequestMethod.POST)
     public Answer answerQuestion(@RequestParam(value="num") int num, @RequestParam(value="ans") int ans) {
         boolean userAnswer = (game.getQuestion(num).answer(ans));
@@ -28,6 +27,7 @@ public class RESTController {
         return new Answer(userAnswer, game.getQuestion(num).getCorrectCity());
     }
 
+    @CrossOrigin
     @RequestMapping(path="/getScore", method=RequestMethod.GET)
     public Score getScore() {
         return new Score(game.getQuestionLib().getScore());
